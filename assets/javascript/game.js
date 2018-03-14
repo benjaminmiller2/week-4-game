9863dd1374d2647c017c4fd7d4d47edda95863d9
Benjamin Miller <benjamin.miller2@northwestern.edu>
2018-03-13 22:08:36 -0500

    3/13

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,96 +1,77 @@
-//generate random number between 19 and 120 
-let randomNumber = (Math.floor(Math.random() * 101) + 20);
 
-//create variable for player score, and set to zero
-let playerScore = 0;
-
-//create empty variable to hold player input
-let input;
-
-//create varialbes for wins and losses
 let win = 0;
 let lose = 0;
-//crystal buttons
-  //assign value to buttons 
-let crystalOne = 12;
-let crystalTwo = 1;
-let crystalThree = 9;
-let crystalFour = 4;
+let number = 0;
+let playerScore = 0;
+let score = [];
+let input;
+let randomNumber = [];
+let values = [12, 1, 4, 7]; 
+let evalNumber = [];
 
-//display player score
+$("#number").text(number);
 $("#playerScore").text(playerScore);
-
-//display win/lose
 $("#wins").text(win);
 $("#losses").text(lose);
-
-//create function to add player input to player score
-function add(){
-  playerScore = playerScore + input;
-}
-
-//reset game    
-function reset(){
-  playerScore = 0;
-  $("#number").text("");
-   $("#playerScore").text(0);
-  start();
-}
-
-//function to initiate game
-function startGame() {
-
-  //display random number
-  $("#number").text(randomNumber);
-
-    //create functionality for buttons
-$("#crystalOne").on("click", function(){
-  input = crystalOne;
-  add();
-  console.log (playerScore);
-  $("#playerScore").text(playerScore);
-});
-
-$("#crystalTwo").on("click", function(){
-  input = crystalTwo;
- add();
-  console.log (playerScore);
-  $("#playerScore").text(playerScore);
-});
-
-$("#crystalThree").on("click", function(){
-  input = crystalThree;
-  add();
-  console.log (playerScore);
-  $("#playerScore").text(playerScore);
-});
-
-$("#crystalFour").on("click", function(){
-  input = crystalFour;
-  add();
-  console.log (playerScore);
-  $("#playerScore").text(playerScore);
-});
-
-  //set conditions for win/lose
-
-  //if equal random number, then win
-  if ((playerScore) === (randomNumber)){
-    win++;
-    //display win
-    $("#wins").text(win);
-    console.log("You've won " + win + " games.");
-  }
-  //if over random number, then lose
-  if ((playerScore) > (randomNumber)){
-    lose++;
-    //display loss
-    $("#losses").text(lose);
-    console.log("You've lost " + lose + " games.");
-  } 
-}
-
  
+function rando(){ 
+  let value = Math.floor(Math.random() * 101) + 20;
+  randomNumber.push(value);
+  $("#number").text(randomNumber)
+};
+
+function startGame(){ 
+  randomNumber=[];
+  score=[];
+  evalNumber=[];
+  rando();
+  number = randomNumber;
+  $("#number").text(number);
+};
+
+function add(){
+   playerScore = playerScore + input;
+};
+
+function eval(){
+  let a = randomNumber;
+  let b = playerScore;
   
+if ( a === b ){
+  win++;
+  console.log("youve won " + win + " games");
+  $("#wins").text(win);
+};
+ 
+if ( a < b ){
+  lose++;
+  console.log("youve lost " + lose + " games");
+  $("#losses").text(lose);
+};
+};
 
 
+$("#button1").on("click", function(){
+  input = values[0];
+  add();
+  eval();
+  $("#playerScore").text(playerScore);
+})
+$("#button2").on("click", function(){
+  input = values[1];
+  add();
+  eval();
+  $("#playerScore").text(playerScore);
+})
+$("#button3").on("click", function(){
+  input = values[2];
+  add();
+  eval();
+  $("#playerScore").text(playerScore);
+})
+$("#button4").on("click", function(){
+  input = values[3];
+  add();
+  eval();
+  $("#playerScore").text(playerScore);
+});
