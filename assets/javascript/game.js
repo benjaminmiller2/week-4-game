562b8294410b19bbb7177b74dab1d14c9b877f97
Benjamin Miller <benjamin.miller2@northwestern.edu>
2018-03-14 11:02:56 -0500

    3/14

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,15 @@
-
 let win = 0;
 let lose = 0;
 let number = 0;
 let playerScore = 0;
-let score = [];
 let input;
 let randomNumber = [];
 let values = [12, 1, 4, 7]; 
-let evalNumber = [];
+
 
 $("#number").text(number);
-$("#playerScore").text(playerScore);
-$("#wins").text(win);
-$("#losses").text(lose);
+
+
  
 function rando(){ 
   let value = Math.floor(Math.random() * 101) + 20;
@@ -21,54 +18,75 @@ function rando(){
 };
 
 function startGame(){ 
+  $("#wins").text(win);
+  $("#losses").text(lose);
+  $("#playerScore").text(playerScore);
   randomNumber=[];
-  score=[];
-  evalNumber=[];
   rando();
   number = randomNumber;
   $("#number").text(number);
 };
 
+function resetGame(){
+  win=0;
+  lose=0;
+  $("#randomNumber").text(0);
+}
+
 function add(){
    playerScore = playerScore + input;
+
 };
 
 function eval(){
-  let a = randomNumber;
+  let a = number;
   let b = playerScore;
   
-if ( a === b ){
-  win++;
-  console.log("youve won " + win + " games");
-  $("#wins").text(win);
-};
- 
-if ( a < b ){
+if ( a > b ){
+  console.log("youve won nothing yet");
+}
+else if ( a < b ){
   lose++;
   console.log("youve lost " + lose + " games");
   $("#losses").text(lose);
-};
+  playerScore = 0;
+}
+  else {
+  win++;
+  console.log("youve won " + win + " games");
+  $("#wins").text(win);
+  playerScore = 0;
+} 
 };
 
+function resetGame(){
+  $("#wins").text(0);
+  $("#losses").text(0);
+  $("#playerScore").text(0);
+  $("#number").text(0);
+}
 
 $("#button1").on("click", function(){
   input = values[0];
   add();
   eval();
   $("#playerScore").text(playerScore);
-})
+});
+
 $("#button2").on("click", function(){
   input = values[1];
   add();
   eval();
   $("#playerScore").text(playerScore);
-})
+});
+
 $("#button3").on("click", function(){
   input = values[2];
   add();
   eval();
   $("#playerScore").text(playerScore);
-})
+});
+
 $("#button4").on("click", function(){
   input = values[3];
   add();
